9addf5afd7d881b70502d6c29b0bf0d1be1db254
Sehi L'Yi <sehilyi@gmail.com>
2022-04-28 17:37:18 -0400

    chore: fix using correct thumbnails

--- src/data/samples.ts
+++ src/data/samples.ts
@@ -46,7 +46,7 @@ const samples: SampleType[] = [
         cnv: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/84ca6ab0-9edc-4636-9d27-55cdba334d7d.consensus.20170119.somatic.cna.annotated.txt',
         bam: 'https://s3.amazonaws.com/gosling-lang.org/data/PCAWG.00e7f3bd-5c87-40c2-aeb6-4e4ca4a8e720.bam',
         bai: 'https://s3.amazonaws.com/gosling-lang.org/data/PCAWG.00e7f3bd-5c87-40c2-aeb6-4e4ca4a8e720.bam.bai',
-        thumbnail: THUMBNAILS[92]
+        thumbnail: THUMBNAILS[93]
     },
     {
         id: '7d332cb1-ba25-47e4-8bf8-d25e14f40d59',
@@ -56,7 +56,7 @@ const samples: SampleType[] = [
         cnv: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/7d332cb1-ba25-47e4-8bf8-d25e14f40d59.consensus.20170119.somatic.cna.annotated.txt',
         bam: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/PCAWG.27a255b4-0479-4aff-95ae-9d23c6975739.bam',
         bai: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/PCAWG.27a255b4-0479-4aff-95ae-9d23c6975739.bam.bai',
-        thumbnail: THUMBNAILS[92]
+        thumbnail: THUMBNAILS[94]
     },
     {
         id: '9ae0744a-9bc1-4cd7-b7cf-c6569ed9e4aa',
@@ -66,7 +66,7 @@ const samples: SampleType[] = [
         cnv: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/9ae0744a-9bc1-4cd7-b7cf-c6569ed9e4aa.consensus.20170119.somatic.cna.annotated.txt',
         bam: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/PCAWG.251466ea-c72f-4004-900f-e6b2d667f7ea.bam',
         bai: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/PCAWG.251466ea-c72f-4004-900f-e6b2d667f7ea.bam.bai',
-        thumbnail: THUMBNAILS[92]
+        thumbnail: THUMBNAILS[95]
     },
     {
         id: 'b27d75ba-5989-4200-bfe9-f1b7d7cf8008',
@@ -76,7 +76,7 @@ const samples: SampleType[] = [
         cnv: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/b27d75ba-5989-4200-bfe9-f1b7d7cf8008.consensus.20170119.somatic.cna.annotated.txt',
         bam: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/PCAWG.c8e7bbdb-6d87-445f-bf43-dbf88805b1ed.bam',
         bai: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/PCAWG.c8e7bbdb-6d87-445f-bf43-dbf88805b1ed.bam.bai',
-        thumbnail: THUMBNAILS[92]
+        thumbnail: THUMBNAILS[96]
     },
     {
         id: 'fc8edf46-2005-1af4-e040-11ac0d481414',
@@ -86,7 +86,7 @@ const samples: SampleType[] = [
         cnv: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/fc8edf46-2005-1af4-e040-11ac0d481414.consensus.20170119.somatic.cna.annotated.txt',
         bam: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/4bcf4f050a418a10e324fae4392a7ebc.bam',
         bai: 'https://s3.amazonaws.com/gosling-lang.org/data/SV/4bcf4f050a418a10e324fae4392a7ebc.bam.bai',
-        thumbnail: THUMBNAILS[92]
+        thumbnail: THUMBNAILS[97]
     },
     {
         id: 'SRR7890905',
@@ -96,7 +96,7 @@ const samples: SampleType[] = [
         cnv: 'https://gist.githubusercontent.com/sehilyi/e3e19907a3d00638e8d3d0b0ddb56a26/raw/1ab8177946ec91394cb697621497acd7b9a513ff/SRR7890943.ascat.cnv.tsv',
         bam: 'https://somatic-browser-test.s3.amazonaws.com/SRR7890905_GAPFI2USVS21.bam',
         bai: 'https://somatic-browser-test.s3.amazonaws.com/SRR7890905_GAPFI2USVS21.bam.bai',
-        thumbnail: THUMBNAILS[92]
+        thumbnail: THUMBNAILS[98]
     }
 ];
 
