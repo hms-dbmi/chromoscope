c8e5bcc3a683231461d3d93e9795cb82ff1d9a98
sehilyi <sehilyi@gmail.com>
2022-08-29 19:39:35 -0400

    chore: add more samples

--- src/data/external-thumbnails.ts
+++ src/data/external-thumbnails.ts
@@ -76,6 +76,87 @@ import _706868b5_4920_433a_bea3_92e9bf83059b from '../script/img/706868b5-4920-4
 import _987528ac_437a_4eb8_a335_4f2076d5c006 from '../script/img/987528ac-437a-4eb8-a335-4f2076d5c006.jpeg';
 import _9bb9164e_ea1b_49ab_bbcc_f59cf8c32345 from '../script/img/9bb9164e-ea1b-49ab-bbcc-f59cf8c32345.jpeg';
 
+// Reference: https://somatic-browser-test.s3.amazonaws.com/atm_bi/configs/atm_bi.all.config.json
+import _0cf9bbc2_cbd5_4b64_8d90_cfa416307b39 from '../script/img/0cf9bbc2-cbd5-4b64-8d90-cfa416307b39.jpeg';
+import _16d5519e_ecb9_4fc8_81f1_e0e4adf722a8 from '../script/img/16d5519e-ecb9-4fc8-81f1-e0e4adf722a8.jpeg';
+import _4b8c6b0c_0d0c_4834_a2f3_691e0919b902 from '../script/img/4b8c6b0c-0d0c-4834-a2f3-691e0919b902.jpeg';
+import _5174f450_0bf7_4237_a53e_844064f0e701 from '../script/img/5174f450-0bf7-4237-a53e-844064f0e701.jpeg';
+import _571ce258_b453_4cac_b05e_05755791be84 from '../script/img/571ce258-b453-4cac-b05e-05755791be84.jpeg';
+import _5d6ad982_bb01_4233_b8fa_d129460eec79 from '../script/img/5d6ad982-bb01-4233-b8fa-d129460eec79.jpeg';
+import _66b4a1f3_480f_4050_96ac_920c1c080864 from '../script/img/66b4a1f3-480f-4050-96ac-920c1c080864.jpeg';
+import _786fc3e4_e2bf_4914_9251_41c800ebb2fa from '../script/img/786fc3e4-e2bf-4914-9251-41c800ebb2fa.jpeg';
+import _99e7016a_c3c1_431c_8838_beb78a3cc017 from '../script/img/99e7016a-c3c1-431c-8838-beb78a3cc017.jpeg';
+import _a9a240f3_d237_4bb8_b968_e4a3cc7c2633 from '../script/img/a9a240f3-d237-4bb8-b968-e4a3cc7c2633.jpeg';
+import _aacaba97_ecde_440b_aae0_0e9c608c4b24 from '../script/img/aacaba97-ecde-440b-aae0-0e9c608c4b24.jpeg';
+import _b37d6283_6f95_4975_a794_f3d5c4bbc7b3 from '../script/img/b37d6283-6f95-4975-a794-f3d5c4bbc7b3.jpeg';
+import _b5cabba2_30a4_458e_897c_00ec3fefa6d2 from '../script/img/b5cabba2-30a4-458e-897c-00ec3fefa6d2.jpeg';
+import _d182b67c_c622_11e3_bf01_24c6515278c0 from '../script/img/d182b67c-c622-11e3-bf01-24c6515278c0.jpeg';
+import _d5423a93_0a72_43cf_a7ae_9011f47553c7 from '../script/img/d5423a93-0a72-43cf-a7ae-9011f47553c7.jpeg';
+import _d8d5585d_32cd_4ac4_b410_a4122a17a558 from '../script/img/d8d5585d-32cd-4ac4-b410-a4122a17a558.jpeg';
+import _d8de6de6_4cfe_4239_9ebd_3437aa65efb2 from '../script/img/d8de6de6-4cfe-4239-9ebd-3437aa65efb2.jpeg';
+import _dc57c2e7_4c5b_4e2b_93c6_f032fcebc2fa from '../script/img/dc57c2e7-4c5b-4e2b-93c6-f032fcebc2fa.jpeg';
+import _ee5d5e7d_78cf_4a29_a9ee_56aa3da877dd from '../script/img/ee5d5e7d-78cf-4a29-a9ee-56aa3da877dd.jpeg';
+import _f9c0a12c_2651_d9ea_e040_11ac0d483201 from '../script/img/f9c0a12c-2651-d9ea-e040-11ac0d483201.jpeg';
+import _ffdd4feb_aca3_4104_b1e8_954d705a6450 from '../script/img/ffdd4feb-aca3-4104-b1e8-954d705a6450.jpeg';
+
+// Reference: https://somatic-browser-test.s3.amazonaws.com/ccne1_amp/configs/ccne1_amp.all.config.json
+import _0009b464_b376_4fbc_8a56_da538269a02f from '../script/img/0009b464-b376-4fbc-8a56-da538269a02f.jpeg';
+import _052665d1_ab75_4f40_be5a_b88154c8beed from '../script/img/052665d1-ab75-4f40-be5a-b88154c8beed.jpeg';
+import _09497b9b_6fca_48cb_af97_161a3e434a51 from '../script/img/09497b9b-6fca-48cb-af97-161a3e434a51.jpeg';
+import _129de5b2_d9b0_4762_9ef8_72d98231fb50 from '../script/img/129de5b2-d9b0-4762-9ef8-72d98231fb50.jpeg';
+import _153a64ee_6322_4f2e_b3df_77ef7d3a4cf0 from '../script/img/153a64ee-6322-4f2e-b3df-77ef7d3a4cf0.jpeg';
+import _16df7888_2480_4394_8856_d57a6ef371d2 from '../script/img/16df7888-2480-4394-8856-d57a6ef371d2.jpeg';
+import _17ed8831_a261_42d9_8ff3_cf75a6cb2a24 from '../script/img/17ed8831-a261-42d9-8ff3-cf75a6cb2a24.jpeg';
+import _19c1c97f_a3ec_44a8_8a20_6f97caed1a4f from '../script/img/19c1c97f-a3ec-44a8-8a20-6f97caed1a4f.jpeg';
+import _1c0e384f_7254_4afe_93c0_b3fc6c6a7894 from '../script/img/1c0e384f-7254-4afe-93c0-b3fc6c6a7894.jpeg';
+import _241abdfd_e6de_4830_a233_4bbc5f622725 from '../script/img/241abdfd-e6de-4830-a233-4bbc5f622725.jpeg';
+import _28e81540_4744_4865_b627_c7c9d8a3c2b8 from '../script/img/28e81540-4744-4865-b627-c7c9d8a3c2b8.jpeg';
+import _369c06f2_8904_49cb_99d1_dd297ed0cd0c from '../script/img/369c06f2-8904-49cb-99d1-dd297ed0cd0c.jpeg';
+import _37522f18_77b2_4414_8df8_3c2c8048adba from '../script/img/37522f18-77b2-4414-8df8-3c2c8048adba.jpeg';
+import _3a6bce45_0431_49d8_82df_b1d9a738e5a6 from '../script/img/3a6bce45-0431-49d8-82df-b1d9a738e5a6.jpeg';
+import _3db3b7b1_da1d_4b9c_a92a_c60fecf4328c from '../script/img/3db3b7b1-da1d-4b9c-a92a-c60fecf4328c.jpeg';
+import _441d2f21_b448_4fc8_8c54_9b85aca56237 from '../script/img/441d2f21-b448-4fc8-8c54-9b85aca56237.jpeg';
+import _493e7008_551c_4c0e_b567_a0f31868629e from '../script/img/493e7008-551c-4c0e-b567-a0f31868629e.jpeg';
+import _4c8afa82_c623_11e3_bf01_24c6515278c0 from '../script/img/4c8afa82-c623-11e3-bf01-24c6515278c0.jpeg';
+import _504d4cb0_d2dd_420d_82e6_9ec14434a0fc from '../script/img/504d4cb0-d2dd-420d-82e6-9ec14434a0fc.jpeg';
+import _5255d645_d3d2_422e_8449_046cd21f4935 from '../script/img/5255d645-d3d2-422e-8449-046cd21f4935.jpeg';
+import _569393c8_e2fe_4580_a45b_81f1b1e01135 from '../script/img/569393c8-e2fe-4580-a45b-81f1b1e01135.jpeg';
+import _5b560f4c_d2a3_43fa_b394_abef78bdefc1 from '../script/img/5b560f4c-d2a3-43fa-b394-abef78bdefc1.jpeg';
+import _5cc7c966_499e_49e3_b509_4eeb00ee03d4 from '../script/img/5cc7c966-499e-49e3-b509-4eeb00ee03d4.jpeg';
+import _60525b38_c34b_479b_86d3_013c7e567b8f from '../script/img/60525b38-c34b-479b-86d3-013c7e567b8f.jpeg';
+import _64623aaa_a274_4baa_8b71_958d9c8df733 from '../script/img/64623aaa-a274-4baa-8b71-958d9c8df733.jpeg';
+import _68509ede_3dcf_4a6e_9af0_4a9bb4dfa567 from '../script/img/68509ede-3dcf-4a6e-9af0-4a9bb4dfa567.jpeg';
+import _7410c00d_ed0b_48dc_a4bf_eb76ac613980 from '../script/img/7410c00d-ed0b-48dc-a4bf-eb76ac613980.jpeg';
+import _7456abd5_303e_4e6f_bf4e_47efefc7310f from '../script/img/7456abd5-303e-4e6f-bf4e-47efefc7310f.jpeg';
+import _7c5c2293_3f40_4ae3_8346_6a327b2abf4d from '../script/img/7c5c2293-3f40-4ae3-8346-6a327b2abf4d.jpeg';
+import _7e036d40_b669_4929_a1e2_c4736e01a473 from '../script/img/7e036d40-b669-4929-a1e2-c4736e01a473.jpeg';
+import _82d2ea5f_ebdc_4907_a287_fe837fd6b59c from '../script/img/82d2ea5f-ebdc-4907-a287-fe837fd6b59c.jpeg';
+import _8b28f6d2_4b7d_493b_826e_b119a4fb0cb4 from '../script/img/8b28f6d2-4b7d-493b-826e-b119a4fb0cb4.jpeg';
+import _91f458e6_64b7_454d_a542_b0aa23638fd8 from '../script/img/91f458e6-64b7-454d-a542-b0aa23638fd8.jpeg';
+import _96f337d2_cec4_4484_87cd_ab2109d2bf50 from '../script/img/96f337d2-cec4-4484-87cd-ab2109d2bf50.jpeg';
+import _a2f02ff7_8433_4cb5_9324_34f13edeaca1 from '../script/img/a2f02ff7-8433-4cb5-9324-34f13edeaca1.jpeg';
+import _a330a96e_9897_4605_b5f1_5b5ef45cd365 from '../script/img/a330a96e-9897-4605-b5f1-5b5ef45cd365.jpeg';
+import _af9cfac8_7fbe_49cb_aeca_8d68aea57994 from '../script/img/af9cfac8-7fbe-49cb-aeca-8d68aea57994.jpeg';
+import _b757a040_3c9e_45e7_a045_9cd08db11d62 from '../script/img/b757a040-3c9e-45e7-a045-9cd08db11d62.jpeg';
+import _bbe59385_5f83_43f6_a485_517c860bef6f from '../script/img/bbe59385-5f83-43f6-a485-517c860bef6f.jpeg';
+import _bd2f8133_3e71_4c5e_998f_0c1a9fdfb75f from '../script/img/bd2f8133-3e71-4c5e-998f-0c1a9fdfb75f.jpeg';
+import _c1b44966_0f72_4c4f_8783_ab3ffe7f17b2 from '../script/img/c1b44966-0f72-4c4f-8783-ab3ffe7f17b2.jpeg';
+import _cc19a480_9c66_4ee8_a0c0_921acac02689 from '../script/img/cc19a480-9c66-4ee8-a0c0-921acac02689.jpeg';
+import _ce799e7b_30e7_44a5_a185_3e50d5e059ef from '../script/img/ce799e7b-30e7-44a5-a185-3e50d5e059ef.jpeg';
+import _dc537fcf_d910_4c4b_8af9_e7da429f2633 from '../script/img/dc537fcf-d910-4c4b-8af9-e7da429f2633.jpeg';
+import _e45f3391_2e74_4767_817a_280cebac7c57 from '../script/img/e45f3391-2e74-4767-817a-280cebac7c57.jpeg';
+import _e4aaca83_3ae9_47f6_a975_c144767ad705 from '../script/img/e4aaca83-3ae9-47f6-a975-c144767ad705.jpeg';
+import _e56b0990_ff67_47c1_b9ad_87ef1d4aa2ae from '../script/img/e56b0990-ff67-47c1-b9ad-87ef1d4aa2ae.jpeg';
+import _e7d74d34_3255_4c20_90fd_b105e6e229c8 from '../script/img/e7d74d34-3255-4c20-90fd-b105e6e229c8.jpeg';
+import _f393bb0c_4178_ca4a_e040_11ac0d48455f from '../script/img/f393bb0c-4178-ca4a-e040-11ac0d48455f.jpeg';
+import _f82d213f_bc99_5b1d_e040_11ac0c486880 from '../script/img/f82d213f-bc99-5b1d-e040-11ac0c486880.jpeg';
+import _f8407106_8a96_8f85_e040_11ac0d481656 from '../script/img/f8407106-8a96-8f85-e040-11ac0d481656.jpeg';
+import _f8425828_5b0f_23a8_e040_11ac0d484128 from '../script/img/f8425828-5b0f-23a8-e040-11ac0d484128.jpeg';
+import _f85397dc_e6e9_4f54_e040_11ac0d48706c from '../script/img/f85397dc-e6e9-4f54-e040-11ac0d48706c.jpeg';
+import _f856fa85_fdb8_c0b0_e040_11ac0d480b4e from '../script/img/f856fa85-fdb8-c0b0-e040-11ac0d480b4e.jpeg';
+import _f86e2d80_911b_7a19_e040_11ac0d486900 from '../script/img/f86e2d80-911b-7a19-e040-11ac0d486900.jpeg';
+import _fca6150f_d555_a29e_e040_11ac0d4873b2 from '../script/img/fca6150f-d555-a29e-e040-11ac0d4873b2.jpeg';
+
 export const INTERNAL_SAVED_THUMBNAILS = {
     'd392ded3-afc8-4c79-b278-40245f18f2f8': _d392ded3_afc8_4c79_b278_40245f18f2f8,
     'fc8130e5-18b8-ef38-e040-11ac0c483297': _fc8130e5_18b8_ef38_e040_11ac0c483297,
@@ -149,5 +230,82 @@ export const INTERNAL_SAVED_THUMBNAILS = {
     '45e16b70-c3ec-493e-86d1-505ffdf5056c': _45e16b70_c3ec_493e_86d1_505ffdf5056c,
     '706868b5-4920-433a-bea3-92e9bf83059b': _706868b5_4920_433a_bea3_92e9bf83059b,
     '987528ac-437a-4eb8-a335-4f2076d5c006': _987528ac_437a_4eb8_a335_4f2076d5c006,
-    '9bb9164e-ea1b-49ab-bbcc-f59cf8c32345': _9bb9164e_ea1b_49ab_bbcc_f59cf8c32345
+    '9bb9164e-ea1b-49ab-bbcc-f59cf8c32345': _9bb9164e_ea1b_49ab_bbcc_f59cf8c32345,
+    '0cf9bbc2-cbd5-4b64-8d90-cfa416307b39': _0cf9bbc2_cbd5_4b64_8d90_cfa416307b39,
+    '16d5519e-ecb9-4fc8-81f1-e0e4adf722a8': _16d5519e_ecb9_4fc8_81f1_e0e4adf722a8,
+    '4b8c6b0c-0d0c-4834-a2f3-691e0919b902': _4b8c6b0c_0d0c_4834_a2f3_691e0919b902,
+    '5174f450-0bf7-4237-a53e-844064f0e701': _5174f450_0bf7_4237_a53e_844064f0e701,
+    '571ce258-b453-4cac-b05e-05755791be84': _571ce258_b453_4cac_b05e_05755791be84,
+    '5d6ad982-bb01-4233-b8fa-d129460eec79': _5d6ad982_bb01_4233_b8fa_d129460eec79,
+    '66b4a1f3-480f-4050-96ac-920c1c080864': _66b4a1f3_480f_4050_96ac_920c1c080864,
+    '786fc3e4-e2bf-4914-9251-41c800ebb2fa': _786fc3e4_e2bf_4914_9251_41c800ebb2fa,
+    '99e7016a-c3c1-431c-8838-beb78a3cc017': _99e7016a_c3c1_431c_8838_beb78a3cc017,
+    'a9a240f3-d237-4bb8-b968-e4a3cc7c2633': _a9a240f3_d237_4bb8_b968_e4a3cc7c2633,
+    'aacaba97-ecde-440b-aae0-0e9c608c4b24': _aacaba97_ecde_440b_aae0_0e9c608c4b24,
+    'b37d6283-6f95-4975-a794-f3d5c4bbc7b3': _b37d6283_6f95_4975_a794_f3d5c4bbc7b3,
+    'b5cabba2-30a4-458e-897c-00ec3fefa6d2': _b5cabba2_30a4_458e_897c_00ec3fefa6d2,
+    'd182b67c-c622-11e3-bf01-24c6515278c0': _d182b67c_c622_11e3_bf01_24c6515278c0,
+    'd5423a93-0a72-43cf-a7ae-9011f47553c7': _d5423a93_0a72_43cf_a7ae_9011f47553c7,
+    'd8d5585d-32cd-4ac4-b410-a4122a17a558': _d8d5585d_32cd_4ac4_b410_a4122a17a558,
+    'd8de6de6-4cfe-4239-9ebd-3437aa65efb2': _d8de6de6_4cfe_4239_9ebd_3437aa65efb2,
+    'dc57c2e7-4c5b-4e2b-93c6-f032fcebc2fa': _dc57c2e7_4c5b_4e2b_93c6_f032fcebc2fa,
+    'ee5d5e7d-78cf-4a29-a9ee-56aa3da877dd': _ee5d5e7d_78cf_4a29_a9ee_56aa3da877dd,
+    'f9c0a12c-2651-d9ea-e040-11ac0d483201': _f9c0a12c_2651_d9ea_e040_11ac0d483201,
+    'ffdd4feb-aca3-4104-b1e8-954d705a6450': _ffdd4feb_aca3_4104_b1e8_954d705a6450,
+    '0009b464-b376-4fbc-8a56-da538269a02f': _0009b464_b376_4fbc_8a56_da538269a02f,
+    '052665d1-ab75-4f40-be5a-b88154c8beed': _052665d1_ab75_4f40_be5a_b88154c8beed,
+    '09497b9b-6fca-48cb-af97-161a3e434a51': _09497b9b_6fca_48cb_af97_161a3e434a51,
+    '129de5b2-d9b0-4762-9ef8-72d98231fb50': _129de5b2_d9b0_4762_9ef8_72d98231fb50,
+    '153a64ee-6322-4f2e-b3df-77ef7d3a4cf0': _153a64ee_6322_4f2e_b3df_77ef7d3a4cf0,
+    '16df7888-2480-4394-8856-d57a6ef371d2': _16df7888_2480_4394_8856_d57a6ef371d2,
+    '17ed8831-a261-42d9-8ff3-cf75a6cb2a24': _17ed8831_a261_42d9_8ff3_cf75a6cb2a24,
+    '19c1c97f-a3ec-44a8-8a20-6f97caed1a4f': _19c1c97f_a3ec_44a8_8a20_6f97caed1a4f,
+    '1c0e384f-7254-4afe-93c0-b3fc6c6a7894': _1c0e384f_7254_4afe_93c0_b3fc6c6a7894,
+    '241abdfd-e6de-4830-a233-4bbc5f622725': _241abdfd_e6de_4830_a233_4bbc5f622725,
+    '28e81540-4744-4865-b627-c7c9d8a3c2b8': _28e81540_4744_4865_b627_c7c9d8a3c2b8,
+    '369c06f2-8904-49cb-99d1-dd297ed0cd0c': _369c06f2_8904_49cb_99d1_dd297ed0cd0c,
+    '37522f18-77b2-4414-8df8-3c2c8048adba': _37522f18_77b2_4414_8df8_3c2c8048adba,
+    '3a6bce45-0431-49d8-82df-b1d9a738e5a6': _3a6bce45_0431_49d8_82df_b1d9a738e5a6,
+    '3db3b7b1-da1d-4b9c-a92a-c60fecf4328c': _3db3b7b1_da1d_4b9c_a92a_c60fecf4328c,
+    '441d2f21-b448-4fc8-8c54-9b85aca56237': _441d2f21_b448_4fc8_8c54_9b85aca56237,
+    '493e7008-551c-4c0e-b567-a0f31868629e': _493e7008_551c_4c0e_b567_a0f31868629e,
+    '4c8afa82-c623-11e3-bf01-24c6515278c0': _4c8afa82_c623_11e3_bf01_24c6515278c0,
+    '504d4cb0-d2dd-420d-82e6-9ec14434a0fc': _504d4cb0_d2dd_420d_82e6_9ec14434a0fc,
+    '5255d645-d3d2-422e-8449-046cd21f4935': _5255d645_d3d2_422e_8449_046cd21f4935,
+    '569393c8-e2fe-4580-a45b-81f1b1e01135': _569393c8_e2fe_4580_a45b_81f1b1e01135,
+    '5b560f4c-d2a3-43fa-b394-abef78bdefc1': _5b560f4c_d2a3_43fa_b394_abef78bdefc1,
+    '5cc7c966-499e-49e3-b509-4eeb00ee03d4': _5cc7c966_499e_49e3_b509_4eeb00ee03d4,
+    '60525b38-c34b-479b-86d3-013c7e567b8f': _60525b38_c34b_479b_86d3_013c7e567b8f,
+    '64623aaa-a274-4baa-8b71-958d9c8df733': _64623aaa_a274_4baa_8b71_958d9c8df733,
+    '68509ede-3dcf-4a6e-9af0-4a9bb4dfa567': _68509ede_3dcf_4a6e_9af0_4a9bb4dfa567,
+    '7410c00d-ed0b-48dc-a4bf-eb76ac613980': _7410c00d_ed0b_48dc_a4bf_eb76ac613980,
+    '7456abd5-303e-4e6f-bf4e-47efefc7310f': _7456abd5_303e_4e6f_bf4e_47efefc7310f,
+    '7c5c2293-3f40-4ae3-8346-6a327b2abf4d': _7c5c2293_3f40_4ae3_8346_6a327b2abf4d,
+    '7e036d40-b669-4929-a1e2-c4736e01a473': _7e036d40_b669_4929_a1e2_c4736e01a473,
+    '82d2ea5f-ebdc-4907-a287-fe837fd6b59c': _82d2ea5f_ebdc_4907_a287_fe837fd6b59c,
+    '8b28f6d2-4b7d-493b-826e-b119a4fb0cb4': _8b28f6d2_4b7d_493b_826e_b119a4fb0cb4,
+    '91f458e6-64b7-454d-a542-b0aa23638fd8': _91f458e6_64b7_454d_a542_b0aa23638fd8,
+    '96f337d2-cec4-4484-87cd-ab2109d2bf50': _96f337d2_cec4_4484_87cd_ab2109d2bf50,
+    'a2f02ff7-8433-4cb5-9324-34f13edeaca1': _a2f02ff7_8433_4cb5_9324_34f13edeaca1,
+    'a330a96e-9897-4605-b5f1-5b5ef45cd365': _a330a96e_9897_4605_b5f1_5b5ef45cd365,
+    'af9cfac8-7fbe-49cb-aeca-8d68aea57994': _af9cfac8_7fbe_49cb_aeca_8d68aea57994,
+    'b757a040-3c9e-45e7-a045-9cd08db11d62': _b757a040_3c9e_45e7_a045_9cd08db11d62,
+    'bbe59385-5f83-43f6-a485-517c860bef6f': _bbe59385_5f83_43f6_a485_517c860bef6f,
+    'bd2f8133-3e71-4c5e-998f-0c1a9fdfb75f': _bd2f8133_3e71_4c5e_998f_0c1a9fdfb75f,
+    'c1b44966-0f72-4c4f-8783-ab3ffe7f17b2': _c1b44966_0f72_4c4f_8783_ab3ffe7f17b2,
+    'cc19a480-9c66-4ee8-a0c0-921acac02689': _cc19a480_9c66_4ee8_a0c0_921acac02689,
+    'ce799e7b-30e7-44a5-a185-3e50d5e059ef': _ce799e7b_30e7_44a5_a185_3e50d5e059ef,
+    'dc537fcf-d910-4c4b-8af9-e7da429f2633': _dc537fcf_d910_4c4b_8af9_e7da429f2633,
+    'e45f3391-2e74-4767-817a-280cebac7c57': _e45f3391_2e74_4767_817a_280cebac7c57,
+    'e4aaca83-3ae9-47f6-a975-c144767ad705': _e4aaca83_3ae9_47f6_a975_c144767ad705,
+    'e56b0990-ff67-47c1-b9ad-87ef1d4aa2ae': _e56b0990_ff67_47c1_b9ad_87ef1d4aa2ae,
+    'e7d74d34-3255-4c20-90fd-b105e6e229c8': _e7d74d34_3255_4c20_90fd_b105e6e229c8,
+    'f393bb0c-4178-ca4a-e040-11ac0d48455f': _f393bb0c_4178_ca4a_e040_11ac0d48455f,
+    'f82d213f-bc99-5b1d-e040-11ac0c486880': _f82d213f_bc99_5b1d_e040_11ac0c486880,
+    'f8407106-8a96-8f85-e040-11ac0d481656': _f8407106_8a96_8f85_e040_11ac0d481656,
+    'f8425828-5b0f-23a8-e040-11ac0d484128': _f8425828_5b0f_23a8_e040_11ac0d484128,
+    'f85397dc-e6e9-4f54-e040-11ac0d48706c': _f85397dc_e6e9_4f54_e040_11ac0d48706c,
+    'f856fa85-fdb8-c0b0-e040-11ac0d480b4e': _f856fa85_fdb8_c0b0_e040_11ac0d480b4e,
+    'f86e2d80-911b-7a19-e040-11ac0d486900': _f86e2d80_911b_7a19_e040_11ac0d486900,
+    'fca6150f-d555-a29e-e040-11ac0d4873b2': _fca6150f_d555_a29e_e040_11ac0d4873b2
 };
