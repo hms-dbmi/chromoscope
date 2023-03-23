fd2da4520d6475a145875efeadb99f1362a57620
sehilyi <sehilyi@gmail.com>
2023-03-23 12:52:52 -0400

    chore: use longer names

--- src/ui/cancer-selector.tsx
+++ src/ui/cancer-selector.tsx
@@ -3,197 +3,197 @@ import './side-menu.css';
 
 const PCAWG_SAMPLES = [
     {
-        cancer: 'HCC (Liver)',
+        cancer: 'Liver Hepatocellular Carcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Liver-HCC/configs/Liver-HCC.all.config.json',
         count: 327
     },
     {
-        cancer: 'AdenoCA (Prost)',
+        cancer: 'Prostate Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Prost-AdenoCA/configs/Prost-AdenoCA.all.config.json',
         count: 286
     },
     {
-        cancer: 'AdenoCA (Panc)',
+        cancer: 'Pancreas Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Panc-AdenoCA/configs/Panc-AdenoCA.all.config.json',
         count: 241
     },
     {
-        cancer: 'AdenoCA (Breast)',
+        cancer: 'Breast Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Breast-AdenoCA/configs/Breast-AdenoCA.all.config.json',
         count: 198
     },
     {
-        cancer: 'Medullo (CNS)',
+        cancer: 'Cns Medulloblastoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/CNS-Medullo/configs/CNS-Medullo.all.config.json',
         count: 146
     },
     {
-        cancer: 'RCC (Kidney)',
+        cancer: 'Kidney Renal Cell Carcinoma (Proximal Tubules)',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Kidney-RCC/configs/Kidney-RCC.all.config.json',
         count: 144
     },
     {
-        cancer: 'AdenoCA (Ovary)',
+        cancer: 'Ovary Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Ovary-AdenoCA/configs/Ovary-AdenoCA.all.config.json',
         count: 113
     },
     {
-        cancer: 'Melanoma (Skin)',
+        cancer: 'Skin Melanoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Skin-Melanoma/configs/Skin-Melanoma.all.config.json',
         count: 107
     },
     {
-        cancer: 'BNHL (Lymph)',
+        cancer: 'Lymphoid Mature B-Cell Lymphoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Lymph-BNHL/configs/Lymph-BNHL.all.config.json',
         count: 105
     },
     {
-        cancer: 'AdenoCA (Eso)',
+        cancer: 'Esophagus Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Eso-AdenoCA/configs/Eso-AdenoCA.all.config.json',
         count: 98
     },
     {
-        cancer: 'CLL (Lymph)',
+        cancer: 'Lymphoid Chronic Lymphocytic Leukemia',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Lymph-CLL/configs/Lymph-CLL.all.config.json',
         count: 95
     },
     {
-        cancer: 'PiloAstro (CNS)',
+        cancer: 'Cns Non-Diffuse Glioma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/CNS-PiloAstro/configs/CNS-PiloAstro.all.config.json',
         count: 89
     },
     {
-        cancer: 'Endocrine (Panc)',
+        cancer: 'Pancreas Neuroendocrine Tumor',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Panc-Endocrine/configs/Panc-Endocrine.all.config.json',
         count: 85
     },
     {
-        cancer: 'AdenoCA (Stomach)',
+        cancer: 'Stomach Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Stomach-AdenoCA/configs/Stomach-AdenoCA.all.config.json',
         count: 75
     },
     {
-        cancer: 'AdenoCA (ColoRect)',
+        cancer: 'Colon/Rectum Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/ColoRect-AdenoCA/configs/ColoRect-AdenoCA.all.config.json',
         count: 60
     },
     {
-        cancer: 'SCC (Head)',
+        cancer: 'Head/Neck Squamous Cell Carcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Head-SCC/configs/Head-SCC.all.config.json',
         count: 57
     },
     {
-        cancer: 'MPN (Myeloid)',
+        cancer: 'Myeloid Myeloproliferative Neoplasm',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Myeloid-MPN/configs/Myeloid-MPN.all.config.json',
         count: 51
     },
     {
-        cancer: 'AdenoCA (Uterus)',
+        cancer: 'Uterus Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Uterus-AdenoCA/configs/Uterus-AdenoCA.all.config.json',
         count: 51
     },
     {
-        cancer: 'SCC (Lung)',
+        cancer: 'Lung Squamous Cell Carcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Lung-SCC/configs/Lung-SCC.all.config.json',
         count: 48
     },
     {
-        cancer: 'AdenoCA (Thy)',
+        cancer: 'Thyroid Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Thy-AdenoCA/configs/Thy-AdenoCA.all.config.json',
         count: 48
     },
     {
-        cancer: 'ChRCC (Kidney)',
+        cancer: 'Kidney Renal Cell Carcinoma (Distal Tubules)',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Kidney-ChRCC/configs/Kidney-ChRCC.all.config.json',
         count: 45
     },
     {
-        cancer: 'GBM (CNS)',
+        cancer: 'Cns Diffuse Glioma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/CNS-GBM/configs/CNS-GBM.all.config.json',
         count: 41
     },
     {
-        cancer: 'Osteosarc (Bone)',
+        cancer: 'Bone/Softtissue Sarcoma  Bone',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Bone-Osteosarc/configs/Bone-Osteosarc.all.config.json',
         count: 39
     },
     {
-        cancer: 'AdenoCA (Lung)',
+        cancer: 'Lung Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Lung-AdenoCA/configs/Lung-AdenoCA.all.config.json',
         count: 38
     },
     {
-        cancer: 'AdenoCA (Biliary)',
+        cancer: 'Biliary Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Biliary-AdenoCA/configs/Biliary-AdenoCA.all.config.json',
         count: 34
     },
     {
-        cancer: 'Leiomyo (Bone)',
+        cancer: 'Bone/Softtissue Sarcoma  Soft Tissue',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Bone-Leiomyo/configs/Bone-Leiomyo.all.config.json',
         count: 34
     },
     {
-        cancer: 'TCC (Bladder)',
+        cancer: 'Bladder Transitional Cell Carcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Bladder-TCC/configs/Bladder-TCC.all.config.json',
         count: 23
     },
     {
-        cancer: 'SCC (Cervix)',
+        cancer: 'Cervix Squamous Cell Carcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Cervix-SCC/configs/Cervix-SCC.all.config.json',
         count: 18
     },
     {
-        cancer: 'Oligo (CNS)',
+        cancer: 'Cns Diffuse Glioma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/CNS-Oligo/configs/CNS-Oligo.all.config.json',
         count: 18
     },
     {
-        cancer: 'AML (Myeloid)',
+        cancer: 'Myeloid Acute Myeloid Leukemia',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Myeloid-AML/configs/Myeloid-AML.all.config.json',
         count: 16
     },
     {
-        cancer: 'LobularCA (Breast)',
+        cancer: 'Breast Lobular Carcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Breast-LobularCA/configs/Breast-LobularCA.all.config.json',
         count: 13
     },
     {
-        cancer: 'Epith (Bone)',
+        cancer: 'Bone/Softtissue Bone Neoplasm  Epithelioid',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Bone-Epith/configs/Bone-Epith.all.config.json',
         count: 10
     },
     {
-        cancer: 'Cart (Bone)',
+        cancer: 'Bone/Softtissue Cartilaginous Neoplasm  Benign  Bone',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Bone-Cart/configs/Bone-Cart.all.config.json',
         count: 9
     },
     {
-        cancer: 'Osteoblast (Bone)',
+        cancer: 'Bone/Softtissue Bone Neoplasm  Benign',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Bone-Osteoblast/configs/Bone-Osteoblast.all.config.json',
         count: 5
     },
     {
-        cancer: 'DCIS (Breast)',
+        cancer: 'Breast In Situ Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Breast-DCIS/configs/Breast-DCIS.all.config.json',
         count: 3
     },
     {
-        cancer: 'MDS (Myeloid)',
+        cancer: 'Myeloid Myelodysplastic Syndrome',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Myeloid-MDS/configs/Myeloid-MDS.all.config.json',
         count: 3
     },
     {
-        cancer: 'AdenoCA (Cervix)',
+        cancer: 'Cervix Adenocarcinoma',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Cervix-AdenoCA/configs/Cervix-AdenoCA.all.config.json',
         count: 2
     },
     {
-        cancer: 'NOS (Lymph)',
+        cancer: 'Lymphoid Lymphoma  Nos',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Lymph-NOS/configs/Lymph-NOS.all.config.json',
         count: 2
     },
     {
-        cancer: 'Benign (Bone)',
+        cancer: 'Bone/Softtissue Bone Neoplasm  Benign',
         url: 'https://sehilyi.github.io/goscan/?showSamples=true&external=https://somatic-browser-test.s3.amazonaws.com/PCAWG/Bone-Benign/configs/Bone-Benign.all.config.json',
         count: 1
     }
