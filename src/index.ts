803658c3f3985939c325cbe95694405f82aaa3ac
sehilyi <sehilyi@gmail.com>
2023-05-05 11:08:14 -0400

    chore: remove npm publish

--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as generateSpec } from './main-spec';
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,11 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
-import pkg from './package.json';
 
 // https://vitejs.dev/config/
-const browser = defineConfig({
+export default defineConfig({
     build: { target: 'esnext' },
     optimizeDeps: {
         include: ['gosling.js']
     },
     plugins: [reactRefresh()]
 });
-
-const lib = defineConfig({
-    build: {
-        target: 'esnext',
-        outDir: 'lib',
-        minify: false,
-        sourcemap: true,
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            formats: ['es'],
-            fileName: pkg.name
-        }
-    },
-    optimizeDeps: {
-        include: ['gosling.js']
-    },
-    plugins: [reactRefresh()]
-});
-
-export default ({ command, mode }) => {
-    if (command === 'build' && mode === 'lib') return lib;
-    return browser;
-};
