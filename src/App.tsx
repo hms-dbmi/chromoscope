82c0bd5597eaa154a0adb429dca61c85c647704b
Sehi L'Yi <sehilyi@gmail.com>
2023-05-09 17:36:20 -0400

    feat: add About popup (#109)

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,12 @@ import { EXTERNAL_THUMBNAILS } from './data/stevens-mpnst';
 import CancerSelector from './ui/cancer-selector';
 import HorizontalLine from './ui/horizontal-line';
 import SampleConfigForm from './ui/sample-config-form';
+import { BrowserDatabase } from './browser-log';
 
 const db = new Database();
+const log = new BrowserDatabase();
 
+const DB_DO_NOT_SHOW_ABOUT_BY_DEFAULT = (await log.get())?.doNotShowAboutByDefault ?? false;
 const DATABSE_THUMBNAILS = await db.get();
 const GENERATED_THUMBNAILS = {};
 
@@ -81,6 +84,7 @@ function App(props: RouteComponentProps) {
 
     // interactions
     const [showSamples, setShowSamples] = useState(urlParams.get('showSamples') !== 'false' && !xDomain);
+    const [showAbout, setShowAbout] = useState(!DB_DO_NOT_SHOW_ABOUT_BY_DEFAULT);
     const [thumbnailForceGenerate, setThumbnailForceGenerate] = useState(false);
     const [generateThumbnails, setGenerateThumbnails] = useState(false);
     const [doneGeneratingThumbnails, setDoneGeneratingThumbnails] = useState(false);
@@ -610,7 +614,13 @@ function App(props: RouteComponentProps) {
                     />
                 </svg>
                 <div className="sample-label">
-                    {demo.cancer.charAt(0).toUpperCase() + demo.cancer.slice(1) + ' • ' + demo.id}
+                    <a className="chromoscope-title" href="./">
+                        CHROMOSCOPE
+                    </a>
+                    <span className="dimed">{' | '}</span>
+                    {/* {demo.cancer.charAt(0).toUpperCase() + demo.cancer.slice(1) + ' • ' + demo.id} */}
+                    {demo.cancer.charAt(0).toUpperCase() + demo.cancer.slice(1)}
+                    <small>{demo.id}</small>
                     <span className="title-btn" onClick={() => gosRef.current?.api.exportPng()}>
                         <svg className="button" viewBox="0 0 16 16">
                             <title>Export Image</title>
@@ -693,20 +703,6 @@ function App(props: RouteComponentProps) {
                             <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
                         </svg>
                     </span>
-                    <a
-                        className="title-btn"
-                        href="https://chromoscope.bio/docs/"
-                        target="_blank"
-                        style={{ marginLeft: 140 }}
-                        rel="noreferrer"
-                    >
-                        <svg className="button" viewBox="0 0 16 16">
-                            <title>Open Documentation</title>
-                            <path d="M8.646 5.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 8 8.646 6.354a.5.5 0 0 1 0-.708zm-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 8l1.647-1.646a.5.5 0 0 0 0-.708z" />
-                            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
-                            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
-                        </svg>
-                    </a>
                     {!isChrome() ? (
                         <a
                             style={{
@@ -720,22 +716,60 @@ function App(props: RouteComponentProps) {
                             ⚠️ Chromoscope is optimized for Google Chrome
                         </a>
                     ) : null}
-                </div>
-                {demo.bam && demo.bai ? (
-                    <div className="help-label">
-                        <span
-                            style={{ border: '1.4px solid gray', borderRadius: 10, padding: '0px 6px', margin: '6px' }}
+                    <a
+                        className="title-github-link"
+                        href="https://github.com/hms-dbmi/chromoscope"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <title>GitHub</title>
+                            <path
+                                fill="currentColor"
+                                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                            ></path>
+                        </svg>
+                        GitHub
+                    </a>
+                    <a className="title-doc-link" href="https://chromoscope.bio/docs/" target="_blank" rel="noreferrer">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
                         >
-                            {'?'}
-                        </span>
-                        {'Click on a SV to see alignment around breakpoints'}
-                    </div>
-                ) : null}
+                            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
+                        </svg>
+                        Documentation
+                    </a>
+                    <a
+                        className="title-about-link"
+                        onClick={() => {
+                            setShowAbout(true);
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M5.933.87a2.89 2.89 0 0 1 4.134 0l.622.638.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636zM7.002 11a1 1 0 1 0 2 0 1 1 0 0 0-2 0zm1.602-2.027c.04-.534.198-.815.846-1.26.674-.475 1.05-1.09 1.05-1.986 0-1.325-.92-2.227-2.262-2.227-1.02 0-1.792.492-2.1 1.29A1.71 1.71 0 0 0 6 5.48c0 .393.203.64.545.64.272 0 .455-.147.564-.51.158-.592.525-.915 1.074-.915.61 0 1.03.446 1.03 1.084 0 .563-.208.885-.822 1.325-.619.433-.926.914-.926 1.64v.111c0 .428.208.745.585.745.336 0 .504-.24.554-.627z" />
+                        </svg>
+                        About
+                    </a>
+                </div>
                 <div id="vis-panel" className="vis-panel">
                     <div className={'vis-overview-panel ' + (!showSamples ? 'hide' : '')}>
-                        <div className="title">
-                            Samples
-                            {` (Total of ${filteredSamples.length})`}
+                        <div
+                            className="title"
+                            onClick={() => {
+                                setShowSamples(false);
+                            }}
+                        >
+                            <b>CHROMOSCOPE</b> <span className="dimed">{' | '}</span> Samples
                             <input
                                 type="text"
                                 className="sample-text-box"
@@ -800,6 +834,7 @@ function App(props: RouteComponentProps) {
                                     }}
                                 />
                             </div>
+                            <div className="overview-status">{`Total of ${filteredSamples.length} samples loaded`}</div>
                             <div className="overview-container">{smallOverviewWrapper}</div>
                         </div>
                     </div>
@@ -1082,6 +1117,66 @@ function App(props: RouteComponentProps) {
                         pointerEvents: 'none'
                     }}
                 />
+                <div
+                    className={showAbout ? 'about-modal-container' : 'about-modal-container-hidden'}
+                    onClick={() => setShowAbout(false)}
+                />
+                <div className={showAbout ? 'about-modal' : 'about-modal-hidden'}>
+                    <button className="about-modal-close-button" onClick={() => setShowAbout(false)}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="30"
+                            height="30"
+                            viewBox="0 0 16 16"
+                            strokeWidth="2"
+                            stroke="none"
+                            fill="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
+                        </svg>
+                    </button>
+                    <p>
+                        <b>Chromoscope</b>
+                        <span className="dimed">{' | '}</span>About
+                    </p>
+                    <p>Welcome to the documentation of Chromoscope!</p>
+
+                    <p>
+                        Chromoscope is an interactive visualization tool that supports <b>multiscale</b> and{' '}
+                        <b>multiform</b> visualizations. Chromoscope enables the user to analyze SVs at multiple scales,
+                        using four main views (multiscale). Moreover, each view uses different visual representations
+                        (multiform) that can facilitate the interpretation for a given level of scale.
+                    </p>
+                    <h4>Learn more about Chromoscope</h4>
+                    <ul>
+                        <li>
+                            <b>GitHub:</b>{' '}
+                            <a href="https://github.com/hms-dbmi/chromoscope" target="_blank" rel="noreferrer">
+                                https://github.com/hms-dbmi/chromoscope
+                            </a>
+                        </li>
+                        <li>
+                            <b>Decomentation:</b>{' '}
+                            <a href="https://chromoscope.bio/docs/" target="_blank" rel="noreferrer">
+                                https://chromoscope.bio/docs/
+                            </a>
+                        </li>
+                        <li>
+                            <b>Preprint:</b> TBA{' '}
+                        </li>
+                    </ul>
+                    <button
+                        className="about-modal-disable-button"
+                        onClick={() => {
+                            log.add(true);
+                            setShowAbout(false);
+                        }}
+                    >
+                        Do not show this information by default
+                    </button>
+                </div>
                 <div
                     className="move-to-top-btn"
                     onClick={() => {
