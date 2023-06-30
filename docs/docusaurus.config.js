ad18e15c94b01be8393543941f331be6fe88ea06
sehilyi <sehilyi@gmail.com>
2023-06-30 18:12:54 -0400

    fix: fix branch name in docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/hms-dbmi/chromoscope/tree/main/docs'
+                    editUrl: 'https://github.com/hms-dbmi/chromoscope/tree/master/docs'
                 },
                 // blog: {
                 //   showReadingTime: true,
