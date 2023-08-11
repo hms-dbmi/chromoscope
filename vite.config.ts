f2b3dbdd82e3745de0854b6ac344c3fc55be04c8
sehilyi <sehilyi@gmail.com>
2023-08-11 12:05:58 -0400

    chore: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/app/',
     build: { target: 'esnext' },
     optimizeDeps: {
         include: ['gosling.js']
