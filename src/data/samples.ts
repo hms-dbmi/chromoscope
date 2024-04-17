8b8b944c8355868a7be2e6e0c1c377196d97551a
SEHI L'YI <sehilyi@gmail.com>
2024-04-17 10:07:42 -0400

    feat: make pe_support optional in sv bedpe data (#137)

--- src/data/samples.ts
+++ src/data/samples.ts
@@ -1,4 +1,4 @@
-import { Assembly } from 'gosling.js/dist/src/core/gosling.schema';
+import { Assembly } from 'gosling.js/dist/src/gosling-schema';
 import _7a921087 from '../script/img/7a921087-8e62-4a93-a757-fd8cdbe1eb8f.jpeg';
 import _84ca6ab0 from '../script/img/84ca6ab0-9edc-4636-9d27-55cdba334d7d.jpeg';
 import _7d332cb1 from '../script/img/7d332cb1-ba25-47e4-8bf8-d25e14f40d59.jpeg';
