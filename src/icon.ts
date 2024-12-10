ac558651a89af421787c31fafedd83d6414bf6b9
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2024-12-10 11:20:58 -0500

    -feat: abstract navigation bar -style: update navigation bar -feat: add menu icon -chore: add types

--- src/icon.ts
+++ src/icon.ts
@@ -351,5 +351,15 @@ export const ICONS: Record<string, ICON_INFO> = {
         ],
         stroke: 'currentColor',
         fill: 'none'
+    },
+    MENU: {
+        width: 16,
+        height: 16,
+        viewBox: '0 0 16 16',
+        path: [
+            'M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z'
+        ],
+        stroke: 'currentColor',
+        fill: 'none'
     }
 };
