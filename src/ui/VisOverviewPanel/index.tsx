dc2f3fc835582021d26786cc4a0a6ba2540e6f5e
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-02-12 11:21:49 -0500

    style: adjust close and back icons for overview panel

--- src/ui/VisOverviewPanel/index.tsx
+++ src/ui/VisOverviewPanel/index.tsx
@@ -63,12 +63,8 @@ export const VisOverviewPanel = ({
                             setShowSamples(false);
                         }}
                     >
-                        <svg viewBox="0 0 16 16">
-                            <title>Close</title>
-                            <path
-                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-                                fill="currentColor"
-                            ></path>
+                        <svg className="button" viewBox={ICONS.X_MARK.viewBox}>
+                            <path fill="currentColor" d={ICONS.X_MARK.path[0]} />
                         </svg>
                     </button>
 
