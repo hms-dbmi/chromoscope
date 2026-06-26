1975599742f98f76f694b5c4bbb86469e91a0ad6
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-06-26 12:18:27 -0400

    fix: update MSK link

--- src/App.tsx
+++ src/App.tsx
@@ -336,7 +336,7 @@ function App(props: RouteComponentProps) {
         if (!isMinimalMode || cohortIdFromUrl !== 'MSK SPECTRUM' || cohorts['MSK SPECTRUM']) return;
 
         fetch(
-            'https://genomebrowser-uploads.hms.harvard.edu/data/dg204/SPECTRUM/SPECTRUM_config_with_clinicalInfo_sorted_v4.json'
+            'https://somatic-browser-test.s3.us-east-1.amazonaws.com/SPECTRUM/SPECTRUM_config_with_clinicalInfo_sorted_aws_thumbnail.json'
         )
             .then(res => res.json())
             .then(data => {
